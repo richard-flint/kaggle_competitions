98264f7ccc171cd276f4e0e9a34c2c41bf52fefb
Richard Flint <63592862+richard-flint@users.noreply.github.com>
2020-12-23 17:52:28 +0000

    update

--- titanic/model_1.py
+++ titanic/model_1.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
-
 Summary: This is a script that attempts to predict survivors from Titanic.
 This is the introductory Kaggle competition.
-
 See https://www.kaggle.com/c/titanic
 
 Note: This is currently a work in progress (WIP) and is incomplete.
+
+To do:
+    * Implement using skikit-learn
+    * Implement a random forest
+    * Implement a neural network
+    * Improve/trial different approaches to missing values
+    * Do some data engineering on features like cabin no.
 """
+
+#***********************#
+#*** Clear workspace ***#
+#***********************#
+
+from IPython import get_ipython
+get_ipython().magic('reset -sf')
 
 #************************#
 #*** Import libraries ***#
@@ -18,6 +27,7 @@ Note: This is currently a work in progress (WIP) and is incomplete.
 
 import numpy as np
 import pandas as pd
+import missingno as msno
 import matplotlib.pyplot as plt
 import model_1_functions as udf
 
@@ -27,24 +37,28 @@ import model_1_functions as udf
 
 #Select which features to include in the model (True=Yes, False=No):
 PassengerId=False
-Survived=True     #Note: This is always True
+Survived=False    #Note: This is always False as not a feature
 Pclass=True
 Name=False
 Sex=True
-Age=True
+Age=True        #Note: This feature has missing values
 SibSp=True
 Parch=True
 Ticket=False
 Fare=True
-Cabin=False
+Cabin=False     #Note: This feature has missing values
 Embarked=False
 
-#Save this information in an array
-features_in_out=np.array([PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked])
-
-#Gradient descent parameters
-alpha=3
+#Set gradient descent parameters
+regularisation = 1 #0 = off, 1 = on
+k=1
+alpha=0.001
 number_of_iterations=100000
+
+#Set how to determine missing values in the dataset
+#Options include: remove_columns,remove_rows
+missing_values="remove_columns"
+
 
 #***************************#
 #*** Read and clean data ***#
@@ -53,40 +67,50 @@ number_of_iterations=100000
 #Read data
 data = pd.read_csv("train.csv")
 
-#Shuffle data
+#Plot to see if there is any missing data
+#plt.figure(1)
+#msno.matrix(data)
+
+#Manage missing values
+data,Age,Cabin=udf.manage_missing_values(data,missing_values,Age,Cabin)
+
+#Shuffle data (for dividing into training and test datasets)
 data=data.sample(frac=1)
 
-#Clean data
+#Clean data (e.g. convert categorical (text) to numerical data)
 data=udf.clean_data(data)
 
-#Convert to numpy array
-data_np=data.to_numpy()
-
 #Remove columns that were not selected as feature vectors
-data_np=data_np[:,features_in_out]
+features_in,features_out=udf.features_in_out(PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked)
+data_in=data.drop(columns=features_out)
 
-#Remove rows with blank (nan) values
-data_np=data_np[~np.isnan(data_np).any(axis=1)]
+#Convert data to numpy array
+data_np=data_in.to_numpy()
+
+#**********************#
+#*** Define vectors ***#
+#**********************#
+
+#Define feature vector X
+X=data_np
+
+#Define output vector y
+y=data['Survived'].to_numpy()
 
 #Extract data parameters
 m=data_np.shape[0]
+n=data_np.shape[1]
 
-#Define feature vector X (and number of features n)
-X,n=udf.define_feature_vector(data_np,m)
-
-#Define output vector y
-y=data_np[:,1]
-
-#Divide data into two datasets (one for training, one for test)
+#Divide data into two datasets (one for training, one for testing)
 X_train,X_test,y_train,y_test=udf.divide_data(X,y)
 
-#Extract data parameters
+#Extract data parameters for training and testing datasets
 m_train=X_train.shape[0]       #m = number of rows
 m_test=X_test.shape[0]
 
-#**************************#
-#*** Initialise vectors ***#
-#**************************#
+#***********************************************#
+#*** Initialise vectors for gradient descent ***#
+#***********************************************#
 
 #Initialise theta
 theta=np.zeros(n,dtype='float64')       #initialise theta at 0
@@ -95,7 +119,10 @@ theta=np.zeros(n,dtype='float64')       #initialise theta at 0
 h=udf.calculate_h(X_train,theta)
 
 #Calculate initial cost function
-J=udf.calculate_J(h,y_train,m_train)
+if regularisation == 0:
+    J=udf.calculate_J_without_regularisation(h,y_train,m_train)
+elif regularisation == 1:
+    J=udf.calculate_J_with_regularisation(h,y_train,m_train,k,theta)
 
 #Initialise history vectors
 J_history=np.zeros(number_of_iterations+1)
@@ -105,25 +132,29 @@ theta_history=np.zeros([number_of_iterations+1,n])
 theta_history[0,:]=theta
 J_history[0]=J
 
-#******************************************************#
-#*** Use gradient descent to minimise cost function ***#
-#******************************************************#
+#**********************************#
+#*** Implement gradient descent ***#
+#**********************************#
 
 #Iterate through gradient descent
 for i in range(number_of_iterations):
     theta=udf.update_theta(X_train,y_train,theta,alpha,m_train)
     h=udf.calculate_h(X_train,theta)
-    J=udf.calculate_J(h,y_train,m_train)
+    if regularisation == 0:
+        J=udf.calculate_J_without_regularisation(h,y_train,m_train)
+    elif regularisation == 1:
+        J=udf.calculate_J_with_regularisation(h,y_train,m_train,k,theta)
     J_history[i+1]=J
     theta_history[i+1,:]=theta
 
 #Plot the results
+plt.figure(2)
 plt.plot(J_history)
 #plt.scatter(theta_history[:,0],theta_history[:,1])
 
-#*************************#
-#*** Test on test data ***#
-#*************************#
+#*************************************#
+#*** Predict survival on test data ***#
+#*************************************#
 
 #Cacluate h on test data
 h_test=udf.calculate_h(X_test,theta)
@@ -131,14 +162,51 @@ h_test=udf.calculate_h(X_test,theta)
 #Predict y values for test data
 y_predicted=np.where(h_test>=0.5,1,0)
 
-#Compare predicted against actual
-correct_vs_incorrect=np.where(y_predicted==y_test,1,0)
+#**************************#
+#*** Review performance ***#
+#**************************#
 
-#Calculate percentage correct
-percentage_correct=sum(correct_vs_incorrect)/m_test
+#Calculate confusion matrix
+true_positive_count,true_negative_count,false_positive_count,false_negative_count=udf.calculate_confustion_matrix(y_predicted,y_test)
 
-#Print results
-print(percentage_correct)
+#Calculate performance metrics
+accuracy,recall,precision,specificity,f_measure,TPR,FPR=udf.calculate_performance_metrics(true_positive_count,true_negative_count,false_positive_count,false_negative_count,m_test)
+
+#Print performance metrics
+print("Accuracy = ",round(accuracy,2),"%")
+print("Recall = ",round(recall,2),"%")
+print("Precision = ",round(precision,2),"%")
+print("Specificity = ",round(specificity,2),"%")
+print("F-measure = ",round(f_measure,2),"%")
+print("TPR = ",round(TPR,2),"%")
+print("FPR = ",round(FPR,2),"%")
+
+#Draw ROC and calculate ROC AUC
+number_of_roc_steps=21
+ROC_TPR=np.zeros(number_of_roc_steps,dtype='float64')
+ROC_FPR=np.zeros(number_of_roc_steps,dtype='float64')
+roc_increment=1/(number_of_roc_steps-1)
+threshold=0
+
+for i in range(number_of_roc_steps):
+    #Estimate predicted values using variable threshold
+    y_predicted_roc=np.where(h_test>=threshold,1,0)
+    
+    #Calculate performance
+    roc_true_positive_count,roc_true_negative_count,roc_false_positive_count,roc_false_negative_count=udf.calculate_confustion_matrix(y_predicted_roc,y_test)
+    roc_accuracy,roc_recall,roc_precision,roc_specificity,roc_f_measure,roc_TPR,roc_FPR=udf.calculate_performance_metrics(roc_true_positive_count,roc_true_negative_count,roc_false_positive_count,roc_false_negative_count,m_test)
+    
+    #Record values in vectors
+    ROC_TPR[i]=roc_TPR
+    ROC_FPR[i]=roc_FPR
+    
+    #Increment threshold value
+    threshold=threshold+roc_increment
+    
+#Draw ROC graph
+plt.figure(3)
+plt.step(ROC_FPR,ROC_TPR)
+plt.plot(np.array([0,1]),np.array([0,1]))
 
 #*********************#
 #*** sci-kit learn ***#
--- titanic/model_1_functions.py
+++ titanic/model_1_functions.py
@@ -1,24 +1,40 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec  5 21:32:39 2020
+Summary: This script contains the user defined functions for the main script.
 
-@author: richa
 """
 
 import numpy as np
+
+def manage_missing_values(data,missing_values,Age,Cabin):
+    if missing_values == "remove_columns":
+        Age=False
+        Cabin=False
+    return data,Age,Cabin
+
+def features_in_out(PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked):
+    true_false_array=np.bool_([PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked])
+    list_of_features=np.array(['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked'])
+    features_in=list_of_features[true_false_array]
+    features_out=list_of_features[~true_false_array]
+    return features_in,features_out
 
 def sigmoid(z):
     g=1/(1+np.exp(-z))
     return g
 
-    
 def calculate_h(X, theta):
     z=np.dot(X,theta)
     h=sigmoid(z)
     return h
     
-def calculate_J(h,y,m):
+def calculate_J_without_regularisation(h,y,m):
     J=(1/m)*(np.dot(-y.T,np.log(h))-np.dot((1-y).T,np.log(1-h)))
+    return J
+
+def calculate_J_with_regularisation(h,y,m,k,theta):
+    J=calculate_J_without_regularisation(h,y,m)
+    J=J+((k/(2*m))*np.dot(theta.T,theta))
     return J
 
 def update_theta(X,y,theta,alpha,m):
@@ -32,8 +48,6 @@ def clean_data(data):
     #Clean sex data
     sex_str_to_num = {"Sex":     {"male": 1, "female": 0}}
     data=data.replace(sex_str_to_num)
-    
-    
     return(data)
     
 def divide_data(X,y):
@@ -58,3 +72,93 @@ def define_feature_vector(data_np,m):
     n=X.shape[1]
     
     return X,n
+
+#*******************************************#
+#*** Functions for measuring performance ***#
+#*******************************************#
+
+def identify_true_positives(y_predicted,y_test):
+    true_positives=np.where(y_predicted==0,0,(np.where(y_predicted==y_test,1,0)))
+    return true_positives
+
+def identify_true_negatives(y_predicted,y_test):
+    true_negatives=np.where(y_predicted==1,0,(np.where(y_predicted==y_test,1,0)))
+    return true_negatives
+
+def identify_false_positives(y_predicted,y_test):
+    false_positives=np.where(y_predicted==0,0,(np.where(y_predicted!=y_test,1,0)))
+    return false_positives
+    
+def identify_false_negatives(y_predicted,y_test):
+    false_negatives=np.where(y_predicted==1,0,(np.where(y_predicted!=y_test,1,0)))
+    return false_negatives
+
+def calculate_confustion_matrix(y_predicted,y_test):
+    #Identify TP,TN,FP and FN in data
+    true_positives=identify_true_positives(y_predicted,y_test)
+    true_negatives=identify_true_negatives(y_predicted,y_test)
+    false_positives=identify_false_positives(y_predicted,y_test)
+    false_negatives=identify_false_negatives(y_predicted,y_test)
+    #Count
+    true_positive_count=np.sum(true_positives)
+    true_negative_count=np.sum(true_negatives)
+    false_positive_count=np.sum(false_positives)
+    false_negative_count=np.sum(false_negatives)
+    return true_positive_count,true_negative_count,false_positive_count,false_negative_count
+
+def check_confusion_matrix(y_predicted,y_test,true_positives,true_negatives,false_positives,false_negatives):
+    
+    #Create vector with TP,TN,FP and FN labels
+    confusion_vector=np.where(true_positives==1,'TP',
+                          np.where(true_negatives==1,'TN',
+                          np.where(false_positives==1,'FP',
+                          np.where(false_negatives==1,'FN','NA'))))
+
+    #Create matrix that allows easy checking
+    comparison_matrix=np.vstack([y_predicted,y_test,confusion_vector]).T
+    
+    #Return matrix
+    return comparison_matrix
+
+def calculate_performance_metrics(true_positive_count,true_negative_count,false_positive_count,false_negative_count,m_test):
+    accuracy=calculate_accuracy(true_positive_count,true_negative_count,m_test)
+    recall=calculate_recall(true_positive_count,false_negative_count)
+    precision=calculate_precision(true_positive_count,false_positive_count)
+    specificity=calculate_specificity(true_negative_count,false_positive_count)
+    f_measure=calculate_f_measure(recall,precision)
+    TPR=recall
+    FPR=1-specificity
+    return accuracy,recall,precision,specificity,f_measure,TPR,FPR
+
+def calculate_accuracy(true_positive_count,true_negative_count,m_test):
+    accuracy=(true_positive_count+true_negative_count)/m_test
+    return accuracy
+
+def calculate_recall(true_positive_count,false_negative_count):
+    if true_positive_count==0 and false_negative_count==0:
+        recall=0
+    else:
+        recall=true_positive_count/(true_positive_count+false_negative_count)
+    return recall
+
+def calculate_precision(true_positive_count,false_positive_count):
+    if true_positive_count==0 and false_positive_count==0:
+        precision=0
+    else:
+        precision=true_positive_count/(true_positive_count+false_positive_count)
+    return precision
+
+def calculate_specificity(true_negative_count,false_positive_count):
+    if true_negative_count==0 and false_positive_count==0:
+        specificity=0
+    else:
+        specificity=true_negative_count/(true_negative_count+false_positive_count)
+    return specificity
+
+def calculate_f_measure(recall,precision):
+    if recall==0 and precision==0:
+        f_measure=0
+    else:
+        f_measure=(2*recall*precision)/(recall+precision)
+    return f_measure
+
